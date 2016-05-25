3527ea94dd91a52df457daf122ad634ff86eae2b
debayansen <debayan.s@emamifrankross.com>
2016-05-25 14:54:52 +0530

    Updated gulp and bower files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    concat = require('gulp-concat')
     // jshint = require('gulp-jshint'),
     watch = require('gulp-watch');
+
+gulp.task('concat', function () {
+    gulp.src('bower_components/**/*.js')
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('js/lib/'));
+});
 
 gulp.task('scripts', function(){
     gulp.src('js/**/*.js')
@@ -16,10 +24,18 @@ gulp.task('styles', function(){
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('css/'));
 });
+gulp.task('templates', function(){
+    gulp.src('templates/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('pages/'));
+})
 
 gulp.task('watch', function(){
+    gulp.watch('bower_components/**/*.js',['concat']);
     gulp.watch('js/**/*.js',['scripts']);
     gulp.watch('css/**/*.scss',['styles']);
+    gulp.watch('templates/**/*.jade',['templates']);
+
 });
 
-gulp.task('default', ['scripts','styles','watch']);
+gulp.task('default', ['concat', 'scripts','styles', 'templates', 'watch']);
