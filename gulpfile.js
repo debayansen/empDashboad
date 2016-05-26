f8785ac236a5e2860ef0885497c95398c0c2958b
debayansen <debayan.s@emamifrankross.com>
2016-05-26 15:30:35 +0530

    Updated and fixed the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    jade = require('gulp-jade'),
-    // concat = require('gulp-concat'),
-    // jshint = require('gulp-jshint'),
-    watch = require('gulp-watch');
+'use strict';
+
+var gulp    = require('gulp'),
+    uglify  = require('gulp-uglify'),
+    sass    = require('gulp-sass'),
+    jade    = require('gulp-jade'),
+    watch   = require('gulp-watch');
 
 // gulp.task('concat', function() {
-//     gulp.src('bower_components/**/*.js')
+//     gulp.src('bower_components/**/*.min.js')
 //         .pipe(concat('lib.js'))
 //         .pipe(gulp.dest('js/lib/'));
 // });
@@ -15,28 +15,26 @@ var gulp = require('gulp'),
 gulp.task('scripts', function(){
     gulp.src('js/**/*.js')
         .pipe(uglify())
-        // .pipe(jshint())
-        // .pipe(jshint.reporter('YOUR_REPORTER_HERE'))
         .pipe(gulp.dest('js/'));
 });
+
 gulp.task('styles', function(){
-    gulp.src('css/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    gulp.src('css/*.scss')
+        .pipe(sass())
         .pipe(gulp.dest('css/'));
 });
+
 gulp.task('templates', function(){
     gulp.src('templates/**/*.jade')
         .pipe(jade())
         .pipe(gulp.dest('pages/'));
-})
+});
 
 gulp.task('watch', function(){
-    // gulp.watch('bower_components/**/*.js',['concat']);
-    gulp.watch('js/**/*.js',['scripts']);
-    gulp.watch('css/**/*.scss',['styles']);
     gulp.watch('templates/**/*.jade',['templates']);
+    gulp.watch('css/**/*.scss',['styles']);
+    gulp.watch('js/**/*.js',['scripts']);
 
 });
 
-// gulp.task('default', ['concat', 'scripts','styles', 'templates', 'watch']);
-gulp.task('default', ['scripts','styles', 'templates', 'watch']);
+gulp.task('default', ['styles','scripts','templates','watch']);
