36f89cdf1cf0fd303dd2aaaed56d42582d2e8955
debayansen <debayan.s@emamifrankross.com>
2016-05-31 17:14:10 +0530

    Menu added but not implemented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app=angular.module("frankrossManagement",[])
+var app=angular.module("frankrossManagement",['ngMaterial'])
                 .controller('loginCtrl', function(){
 
                 })
@@ -7,8 +7,38 @@ var app=angular.module("frankrossManagement",[])
                 })
                 .controller('userProfileCtrl', function($scope){
                     $scope.name = "Debayan Sen"
-                });
+                })
+                .controller('BasicDemoCtrl', function DemoCtrl($mdDialog) {
+                    var originatorEv;
+                    this.openMenu = function($mdOpenMenu, ev) {
+                      originatorEv = ev;
+                      $mdOpenMenu(ev);
+                    };
+                    this.notificationsEnabled = true;
+                    this.toggleNotifications = function() {
+                      this.notificationsEnabled = !this.notificationsEnabled;
+                    };
+                    this.redial = function() {
+                      $mdDialog.show(
+                        $mdDialog.alert()
+                          .targetEvent(originatorEv)
+                          .clickOutsideToClose(true)
+                          .parent('body')
+                          .title('Suddenly, a redial')
+                          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+                          .ok('That was easy')
+                      );
+                      originatorEv = null;
+                    };
+                    this.checkVoicemail = function() {
+                      // This never happens.
+                    };
+                })
+                ;
 
 var user = {
-    "name": "Debayan Sen"
+    "empID":"EFRL4984",
+    "name":"Debayan Sen",
+    "position":"Software Engineer",
+    "doj":"01-March-2016"
 }
