2d327fabeab8c8941feaff189350d8b9209a1373
debayansen <debayan.s@emamifrankross.com>
2016-06-14 15:51:56 +0530

    Gulp updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,24 @@ var gulp    = require('gulp'),
     sass    = require('gulp-sass'),
     jade    = require('gulp-jade'),
     rename  = require('gulp-rename'),
-    watch   = require('gulp-watch');
+    sourcemaps   =  require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    watch   = require('gulp-watch')
+;
 
-//For scripts
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 7000
+    });
+});
+
+
+//For min-scripts
 gulp.task('minscripts', function(){
     gulp.src('js/lib/*.min.js')
         .pipe(order([
@@ -33,10 +48,25 @@ gulp.task('scripts', function(){
         .pipe(gulp.dest('js/'));
 });
 
+//For min-styles
+gulp.task('minstyles', function(){
+    gulp.src('css/*.css')
+        .pipe(order([
+            "bootstrap.min.css",
+            "font-awesome.min.js",
+            "owl.carousel.css"
+        ]))
+        .pipe(concat('lib.css'))
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream());
+});
+
 //For Styles
 gulp.task('styles', function(){
     gulp.src('css/style.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css/'));
 });
 
@@ -52,8 +82,10 @@ gulp.task('watch', function(){
     gulp.watch('templates/**/*.jade',['templates']);
     gulp.watch('css/**/*.scss',['styles']);
     gulp.watch('js/**/*.js',['scripts']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+
 
 });
 
 //For Default Task
-gulp.task('default', ['minscripts','styles','scripts','templates','watch']);
+gulp.task('default', ['minscripts','styles','scripts','templates','browser-sync','watch']);
